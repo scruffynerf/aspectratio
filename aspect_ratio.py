08af9a2766717337bcfcc4f46d42702662cc350e
Scruffy Nerf <scruffynerf23@gmail.com>
2022-08-17 20:05:16 -0400

    initial commit

---
+++ aspect_ratio.py
@@ -0,0 +1,177 @@
+import os
+import sys
+import json
+
+try:
+    import stashapi.log as log
+    import stashapi.marker_parse as mp
+    from stashapi.tools import human_bytes
+    from stashapi.types import PhashDistance
+    from stashapi.stashapp import StashInterface
+except ModuleNotFoundError:
+    print("You need to install the stashapp-tools (stashapi) python module. (CLI: pip install stashapp-tools)", file=sys.stderr)
+
+# plugins don't start in the right directory, let's switch to the local directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+if not os.path.exists("config.py"):
+    with open("aspect_ratiodefaults.py", 'r') as default:
+        config_lines = default.readlines()
+    with open("config.py", 'w') as firstrun:
+        firstrun.write("from aspect_ratiodefaults import *\n")
+        for line in config_lines:
+            if not line.startswith("##"):
+                firstrun.write(f"#{line}")
+
+import config
+
+def configfile_edit(configfile, name: str, state: str):
+    found = 0
+    with open(configfile, 'r') as file:
+        config_lines = file.readlines()
+    with open(configfile, 'w') as file_w:
+        for line in config_lines:
+            if name == line.split("=")[0].strip():
+                file_w.write(f"{name} = {state}\n")
+                found += 1
+            elif "#" + name == line.split("=")[0].strip():
+                file_w.write(f"{name} = {state}\n")
+                found += 1
+            else:
+                file_w.write(line)
+        if not found:
+            file_w.write(f"#\n{name} = {state}\n")
+            found = 1
+    return found
+
+def exit_plugin(msg=None, err=None):
+    if msg is None and err is None:
+        msg = "plugin ended"
+    output_json = {"output": msg, "error": err}
+    print(json.dumps(output_json))
+    sys.exit()
+
+def basename(f):
+    f = os.path.normpath(f)
+    return os.path.basename(f)
+
+def getSceneTitle(scene):
+    if scene["title"] != None and scene["title"] != "":
+        return scene["title"]
+    return basename(scene["path"])
+
+def getRatioTags():
+    query = """
+    {
+      findTags(
+        tag_filter: {name: {value: """ + '"' + config.ratio_parent_name + '"' + """ , modifier: EQUALS}}
+        filter: {per_page: -1}
+      ) {
+        count
+        tags {
+          id
+          name
+          children {
+            name
+            id
+          }
+        }
+      }
+    }
+    """
+    results = stash.graphql_query(query)
+    resultschildren = results["findTags"]["tags"][0]["children"]
+    return resultschildren
+
+def get_ids(obj):
+    ids = []
+    for item in obj:
+        ids.append(item['id'])
+    return ids
+
+def get_names(obj):
+    names = []
+    for item in obj:
+        names.append(item['name'])
+    return names
+
+def parent_tag(tag_id):
+    parenttagname = config.ratio_parent_name
+    parent_tag_id = stash.find_tag(parenttagname, create=True).get("id")
+    tag_update = {}
+    tag_update["id"] = tag_id
+    tag_update["parent_ids"] = parent_tag_id
+    stash.update_tag(tag_update)
+    return
+
+def catchup():
+    ratio_names = config.ratiorange.keys()
+    parenttagname = config.ratio_parent_name
+    parent_tag_id = stash.find_tag(parenttagname, create=True).get("id")
+    for tagname in ratio_names:
+        tag_id = stash.find_tag(tagname, create=True).get("id")
+        parent_tag(tag_id)
+    filtertags = {}
+    filtertags["value"] = parent_tag_id
+    filtertags["depth"] = 0
+    filtertags["modifier"] = "EXCLUDES"
+    filter = {}
+    filter["tags"] = filtertags
+    filterlimits = {}
+    filterlimits["per_page"] = 100
+    found = stash.find_scenes(filter, filterlimits)
+    log.debug(found)
+    return None
+
+def main():
+    global stash
+    json_input = json.loads(sys.stdin.read())
+    FRAGMENT_SERVER = json_input["server_connection"]
+    stash = StashInterface(FRAGMENT_SERVER)
+    PLUGIN_ARGS = False
+    HOOKCONTEXT = False
+
+    try:
+        PLUGIN_ARGS = json_input['args']["mode"]
+    except:
+        pass
+
+    if PLUGIN_ARGS:
+        log.debug("--Starting Plugin 'Aspect Ratio'--")
+        if "catchup" in PLUGIN_ARGS:
+            log.info("Catching up with Aspect Ratio tagging on older files")
+            catchup() #loops thru all scenes, and tag
+        exit_plugin("Aspect Ratio plugin finished")
+
+    try:
+        HOOKCONTEXT = json_input['args']["hookContext"]
+    except:
+        exit_plugin("Aspect Ratio hook: No hook context")
+
+    log.debug("--Starting Hook 'Aspect Ratio'--")
+
+    sceneID = HOOKCONTEXT['id']
+    scene = stash.find_scene(sceneID)
+
+    existing_tags = get_names(scene["tags"])
+    height = scene["file"]["height"]
+    width = scene["file"]["width"]
+    ratio = round(width/height,2)
+    for tagname, range in config.ratiorange.items() :
+        if ratio >= range[0] and ratio <=range[1] :
+           if tagname not in existing_tags:
+              tag_ratio = stash.find_tag(tagname, create=True).get("id")
+              parenttag(tag_ratio)
+              stash.update_scenes({
+                'ids': [sceneID],
+                'tag_ids': {
+                    'mode': 'ADD',
+                    'ids': [tag_ratio]
+                }
+              })
+              exit_plugin(f"Aspect Ratio {tagname} matched and set")
+           exit_plugin(f"Aspect Ratio {tagname} matched but already set")
+
+    exit_plugin("Aspect Ratio hook: no ratio matched")
+
+main()
---
+++ aspect_ratiodefaults.py
@@ -0,0 +1,32 @@
+# Ratio Names and Ranges
+
+ratio_parent_name = "Aspect Ratios"
+
+# dont uncomment this first line, unless you want to override and redefine it all (if removing ranges is your goal, this is easier)
+ratiorange = {}
+
+# you can uncomment any of these and adjust values and/or add new values
+# for example: you can replace the single 'Mobile' with 5 unique mobile ratio values, by uncommenting these lines:
+# ratiorange["Ratio: Mobile"] = [0.1, 0.4]
+# ratiorange["Ratio: 6:13 Mobile"] = [0.45, 0.52]
+# ratiorange["Ratio: 9:16 Mobile"] = [0.53, 0.57]
+# ratiorange["Ratio: 3:5 Mobile"] = [0.58, 0.64]
+# ratiorange["Ratio: 2:3 Mobile"] = [0.65, 0.8]
+ratiorange["Ratio: Mobile"] = [0.45, 0.8]
+ratiorange["Ratio: Square"] = [0.81, 1.16]
+ratiorange["Ratio: Old Time Film"] = [1.17, 1.22]
+ratiorange["Ratio: 5:4 Early TV"] = [1.23, 1.27]
+ratiorange["Ratio: 4:3"] = [1.28, 1.35]
+ratiorange["Ratio: 11:8"] = [1.36, 1.4]
+ratiorange["Ratio: IMAX"] = [1.41, 1.47]
+ratiorange["Ratio: 3:2 35mm"] = [1.48, 1.53]
+ratiorange["Ratio: 14:9"] = [1.54, 1.58]
+ratiorange["Ratio: 16:10 or 5:3"] = [1.59, 1.63]
+ratiorange["Ratio: 16:9"] = [1.64, 1.79]
+ratiorange["Ratio: Cinema Widescreen"] = [1.8, 1.89]
+ratiorange["Ratio: Digital IMAX - 4K/2K"] = [1.9, 1.96]
+ratiorange["Ratio: 2:1"] = [1.97, 2.1]
+ratiorange["Ratio: 70mm"] = [2.11, 2.25]
+ratiorange["Ratio: 21:9 Widescreen"] = [2.26, 2.55]
+ratiorange["Ratio: Ultra Panavision 70mm"] = [2.56, 2.99]
+ratiorange["Ratio: 32:9 Widescreen"] = [3.0, 4]
